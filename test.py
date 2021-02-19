fb8c87fcf98c9ff85fd0d277de74eb25d818b305
nayjones <36734894+nayjones@users.noreply.github.com>
2021-02-19 10:40:48 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-this is an example
+#!/bin/python
+
+def search_github(github_token, search):
+    results = []
+    h = {"Authorization":"token "+ github_token}
+    url = "https://api.github.com/search/code?per_page=100&q=" + search  # actual limit of 100
+    r = requests.get( url, headers=h, timeout=30)
+    result = r.json()
+    if 'total_count' in result:
+        log.info("total results: {}".format(result['total_count']))
+        log.warning("total results: {}".format(result['total_count']))
+        #log.error("total results: {}".format(result['total_count']))
+
+    else:
+        log.error("no results found for the search: {}".format(search))
+        
